9c0e7fe0d1b25793cde6736c961dbcb9c2371ddd
ANDR6709 <andreatovar549@gmail.com>
2023-11-12 17:08:53 -0500

    first commit

--- Condicionales.py
+++ Condicionales.py
@@ -1,78 +1,18 @@
-# Ejercicio 1: Verificación de Número Par o Impar
 
-# Escribe un programa que solicite al usuario ingresar un número entero y determine si es par o impar.
+#Ejercicio 2: Clasificación de Edades
 
-numero=int(input('ingrese el numero:'))
-
-if numero % 2== 0:
-    
-    print("el numero",numero,"es par")
-    
-else:
-    print("el numero ",numero,"es impar")
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
+#Crea un programa que pida al usuario su edad y, según la edad proporcionada, muestre uno de los siguientes mensajes:
+#Si la edad es menor de 18 años: "Eres menor de edad."
+#Si la edad está entre 18 y 65 años (incluidos): "Eres un adulto."
+#Si la edad es mayor de 65 años: "Eres un adulto mayor."
 
 
-
-
-
-
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+edad=int(input("ingrese la edad"))
+if edad <18:
+    print("eres menor de edad")
+    if edad >18 and 65:
+        print("eres un adulto")
+    else:
+        if edad >65:
+            print("eres un adulto mayor")
             
-            
-
-    
-    
-   
-    
-    
-    
-    
-    
-   
-
-
-
-
-    
-    
-    
-    
-
-    
-    
-    
-    
-
-
-
-    
-    
-   
-
-
-
- 
- 
-
-
-
-
-
-
-
